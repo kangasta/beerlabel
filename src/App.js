7a8e1e0fe2e8113343250be922ebf22400a853c6
Toni Kangas <toni.ar.kangas@gmail.com>
2019-02-24 18:45:54 +0200

    Start sketching label

--- src/App.js
+++ src/App.js
@@ -9,10 +9,17 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = {data: undefined};
+		this.state = {
+			data: undefined,
+			size: {
+				h: 90,
+				w: 180
+			}
+		};
 
 		this.handleDrop = this.handleDrop.bind(this);
 		this.handleDragover = this.handleDragover.bind(this);
+		this.handleSizeChange = this.handleSizeChange.bind(this);
 	}
 
 	handleDragover(event) {
@@ -36,12 +43,42 @@ class App extends Component {
 		reader.readAsText(files[0]);
 	}
 
+	handleSizeChange(event) {
+		const dimension = event.target.name;
+		const value = event.target.value;
+		if (dimension !== 'h' && dimension !== 'w') return;
+
+		this.setState(old => {
+			old.size[dimension] = value;
+			return old;
+		})
+	}
+
 	render() {
 		// https://www.html5rocks.com/en/tutorials/file/dndfiles/
 		return (
 			<div className='App'>
-				<div className='Droparea' onDragOver={this.handleDragover} onDrop={this.handleDrop}></div>
-				<Label beerData={this.state.data} />
+				<div className='Main' style={{
+					height: this.state.size.h.toString() + 'mm',
+					width: this.state.size.w.toString() + 'mm'
+				}}>
+					<div className='Droparea' onDragOver={this.handleDragover} onDrop={this.handleDrop}>
+						<Label beerData={this.state.data}/>
+					</div>
+				</div>
+				<span className='Controls'>
+					<span className='Field'>
+						<label htmlFor='control-h'>Height:</label>
+						<input type='number' id='control-h' name='h' min='0' max='600' value={this.state.size.h} onChange={this.handleSizeChange}/>
+						<span className='Unit'>mm</span>
+					</span>
+					<span className='Field'>
+						<label htmlFor='control-w'>Width:</label>
+						<input type='number' id='control-w' name='w' min='0' max='600' value={this.state.size.w} onChange={this.handleSizeChange}/>
+						<span className='Unit'>mm</span>
+					</span>
+					<input type='button' name='reset' value='Reset' onClick={() => {this.setState({data: null})}}/>
+				</span>
 			</div>
 		);
 	}
--- src/Label.js
+++ src/Label.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import './Label.css';
+
 class Label extends Component {
+	static toArray(a) {
+		return (Array.isArray(a) ? a : [a]);
+	}
+
 	getGravity(type='OG') {
 		if (this.props.beerData.hasOwnProperty(type)) {
 			return this.props.beerData[type]._text;
@@ -12,12 +18,62 @@ class Label extends Component {
 		}
 	}
 
+	getProperty(property_str) {
+		const property_arr = property_str.split('.');
+		try {
+			return property_arr.reduce((prev, curr) => prev[curr], this.props.beerData);
+		} catch(e) {
+			return undefined;
+		}
+	}
+
+	getMetricsComponent(name, value, unit='') {
+		return (
+			<div className='Metric'>
+				<div className={'Value ' + (!name ? 'Big' : '')}>{value}<span className='Unit'>{unit}</span></div>
+				<div className='Name'>{name}</div>
+			</div>
+		);
+	}
+
+	static getMaltType(malt_name) {
+		if (malt_name.toLowerCase().match(/wheat/)) {
+			return 'Wheat malt';
+		} else if (malt_name.toLowerCase().match(/rye/)) {
+			return 'Rye malt';
+		} else if (malt_name.toLowerCase().match(/oat/)) {
+			return 'Oat malt';
+		} else if (malt_name.toLowerCase().match(/rice/)) {
+			return 'Rice malt';
+		} else if (malt_name.toLowerCase().match(/corn/)) {
+			return 'Corn malt';
+		} else {
+			return 'Barley malt';
+		}
+	}
+
+	getIngredients() {
+		var ing = ['Water'];
+		Label.toArray(this.props.beerData.FERMENTABLES.FERMENTABLE)
+			.forEach(fermentable => {
+				/*
+				if (!fermentable.NAME._text.toLowerCase().match(/malt/)) {
+					ing.push(fermentable.NAME._text);
+				} else {
+				*/
+				const malt = Label.getMaltType(fermentable.NAME._text);
+				if (!ing.includes(malt)) ing.push(malt);
+				//}
+			});
+		ing.push('Hops', 'Yeast');
+		return ing.join(', ')
+	}
+
 	render() {
 		if (!this.props.beerData) {
 			return null;
 		}
 
-		const toArray = a => (Array.isArray(a) ? a : [a]);
 		const abv = Math.round((Number(this.getGravity('OG'))-Number(this.getGravity('FG'))) * 131.25 * 10)/10
 		const IBU = (alpha, amount, time, batch_size, gravity) => {
 			// http://www.realbeer.com/hops/research.html
@@ -29,51 +85,73 @@ class Label extends Component {
 			return bigness * boil_time * added_alpha;
 		}
 
-		const ibu = toArray(this.props.beerData.HOPS.HOP)
+		const ibu = Label.toArray(this.props.beerData.HOPS.HOP)
 			.filter(a => !a.USE._text.toLowerCase().match(/dry.*hop/))
 			.map(a => IBU(a.ALPHA._text/100, a.AMOUNT._text*1000, a.TIME._text, this.props.beerData.BATCH_SIZE._text, this.getGravity('OG')))
-			.reduce((a,b) => Math.round((a+b) * 10) / 10);
+			.reduce((a,b) => Math.round(a + b));
 
 		//http://brewwiki.com/index.php/Estimating_Color
 		const MCU = (grain_color, weigth, volume) => (grain_color * weigth * 2.2046 / (volume / 3.7854))
 		const EBC = (sum_MCUs) => 1.97/* SRM to EBC */ * 1.4922 * Math.pow(sum_MCUs, 0.6859)
 
 		const ebc = Math.round(
-			EBC(toArray(this.props.beerData.FERMENTABLES.FERMENTABLE)
+			EBC(Label.toArray(this.props.beerData.FERMENTABLES.FERMENTABLE)
 			.map(a => MCU(a.COLOR._text, a.AMOUNT._text, this.props.beerData.BATCH_SIZE._text))
 			.reduce((a,b) => Math.round(a+b))))
 
 		return (
-			<div className="App">
-				<div className='Center'>
-					<h1>{this.props.beerData.NAME._text}</h1>
-					<h2>Metrics</h2>
-					<ul>
-						<li>ABV: {abv}</li>
-						<li>IBU: {ibu}</li>
-						<li>EBC: {ebc}</li>
-					</ul>
+			<div className='Label'>
+				<div className='Metrics'>
+					{this.getMetricsComponent('ABV', abv, '%')}
+					{this.getMetricsComponent('IBU', ibu)}
+					{this.getMetricsComponent('EBC', ebc)}
+					{this.getMetricsComponent(null, 50, 'cl')}
+				</div>
+				<div className='Details'>
+					<p className='Ingredients'><span className='Title'>Ingredients:</span>{this.getIngredients()}</p>
+				</div>
+				<div className='LongIngredients'>
+					<h1>Ingredients</h1>
 					<h2>Fermentables</h2>
 					<ul>
-						{toArray(this.props.beerData.FERMENTABLES.FERMENTABLE).map(fermentable => (
+						{Label.toArray(this.props.beerData.FERMENTABLES.FERMENTABLE).map(fermentable => (
 							<li key={fermentable.NAME._text}>{fermentable.NAME._text}: {Math.round(fermentable.AMOUNT._text*10)/10 + ' kg'}</li>
 						))}
 					</ul>
 					<h2>Hops</h2>
 					<ul>
-						{toArray(this.props.beerData.HOPS.HOP).map(hop => (
-							<li key={hop.NAME._text + hop.TIME._text}>{hop.NAME._text}: {hop.AMOUNT._text * 1e3} g, {Math.round(hop.TIME._text)} min</li>
+						{Label.toArray(this.props.beerData.HOPS.HOP).map(hop => (
+							<li key={hop.NAME._text + hop.TIME._text}>{hop.NAME._text}: {hop.AMOUNT._text * 1e3} g, { hop.USE._text.toLowerCase().match(/dry.*hop/) ? 'Dry-hop' : Math.round(hop.TIME._text).toString() + ' min'}</li>
 						))}
 					</ul>
 					<h2>Yeasts</h2>
 					<ul>
-						{toArray(this.props.beerData.YEASTS.YEAST).map(yeast => (
+						{Label.toArray(this.props.beerData.YEASTS.YEAST).map(yeast => (
 							<li key={yeast.NAME._text}>{yeast.NAME._text}: {yeast.AMOUNT._text * 1e3} g</li>
 						))}
 					</ul>
 				</div>
+				<div className='Front'>
+					<div className='Title'>
+						<div className='Name'>{this.getProperty('NAME._text')}</div>
+						<div className='Style'>{this.getProperty('STYLE.NAME._text')}</div>
+					</div>
+				</div>
 			</div>
 		);
+		/*
+		return (
+			<div className='Label'>
+				<h1>{this.props.beerData.NAME._text}</h1>
+				<h2>Metrics</h2>
+				<ul>
+					<li>ABV: {abv}</li>
+					<li>IBU: {ibu}</li>
+					<li>EBC: {ebc}</li>
+				</ul>
+			</div>
+		);
+		*/
 	}
 }
 

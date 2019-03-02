4a85d0c48726754a124eaf81a5dc4a5db2a385e5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-02 23:50:44 +0000

    Deploy kangasta/beerlabel to github.com/kangasta/beerlabel.git:gh-pages

---
+++ precache-manifest.be946d69a14537595cef368e2ce1c650.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccc9ce19f21255397612",
+    "url": "/beerlabel/static/css/main.5c7bb6dc.chunk.css"
+  },
+  {
+    "revision": "ccc9ce19f21255397612",
+    "url": "/beerlabel/static/js/main.ccc9ce19.chunk.js"
+  },
+  {
+    "revision": "cbddb2d263243e93858a",
+    "url": "/beerlabel/static/js/runtime~main.cbddb2d2.js"
+  },
+  {
+    "revision": "663ea29b0cca084e44c6",
+    "url": "/beerlabel/static/js/2.663ea29b.chunk.js"
+  },
+  {
+    "revision": "0126a01dd3073c4a398985291613eef9",
+    "url": "/beerlabel/index.html"
+  }
+];
